b1940edf22767df3f5034f2fca21634f512dbc14
Grant Holle <hollegrant@gmail.com>
2018-05-22 10:56:48 +0800

    Add dotenv properly

--- tests/PowerSchoolTest.php
+++ tests/PowerSchoolTest.php
@@ -4,6 +4,7 @@ namespace GrantHolle\PowerSchool\Tests;
 
 use GrantHolle\PowerSchool\Api\RequestBuilder;
 use PHPUnit\Framework\TestCase;
+use Dotenv\Dotenv;
 
 class PowerSchoolTest extends TestCase
 {
@@ -11,7 +12,7 @@ class PowerSchoolTest extends TestCase
 
     public function setUp()
     {
-        $dotenv = new Dotenv\Dotenv(__DIR__);
+        $dotenv = new Dotenv(__DIR__ . '/../');
         $dotenv->load();
 
         $this->builder = new RequestBuilder(getenv('SERVER'), getenv('CLIENT_ID'), getenv('CLIENT_SECRET'));
