28fc1c7ba0e4c967c58146e0105a41fabb343642
Grant Holle <hollegrant@gmail.com>
2018-05-22 15:11:16 +0800

    Add facade class

---
+++ src/Facades/PowerSchool.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GrantHolle\PowerSchool\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use GrantHolle\PowerSchool\Api\RequestBuilder;
+
+class PowerSchool extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return RequestBuilder::class;
+    }
+}
