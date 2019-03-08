b10fddd3c30186f922b1cd65cb58db24733231c9
Grant Holle <hollegrant@gmail.com>
2019-03-08 13:28:18 +0800

    Implement the migration publications and comment changes

--- src/PowerSchoolServiceProvider.php
+++ src/PowerSchoolServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace GrantHolle\PowerSchool;
 
+use Illuminate\Support\Collection;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use GrantHolle\PowerSchool\Api\RequestBuilder;
 use GrantHolle\PowerSchool\Commands\ClearCache;
@@ -25,15 +27,16 @@ class PowerSchoolServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
-        // Publish the configuration
+        // Publish the configuration and migration
         $this->publishes([
             __DIR__ . '/config.php' => config_path('powerschool.php'),
         ], 'config');
 
-        // Load routes
-        // $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->publishes([
+            __DIR__.'/database/migrations/add_open_id_column_to_users_table.php.stub' => $this->getMigrationFileName($filesystem),
+        ], 'migrations');
 
         // Commands
         if ($this->app->runningInConsole()) {
@@ -51,5 +54,22 @@ class PowerSchoolServiceProvider extends ServiceProvider
     public function provides()
     {
         return [RequestBuilder::class];
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_add_open_id_column_to_users_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_add_open_id_column_to_users_table.php")
+            ->first();
     }
 }
