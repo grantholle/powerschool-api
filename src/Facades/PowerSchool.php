a01965bca3654e06f8fd73fee12271c5507e2a62
Grant Holle <hollegrant@gmail.com>
2020-08-19 14:17:25 +0800

    Add additional functions and clean up request; add tests

--- src/Facades/PowerSchool.php
+++ src/Facades/PowerSchool.php
@@ -2,9 +2,64 @@
 
 namespace GrantHolle\PowerSchool\Api\Facades;
 
+use GrantHolle\PowerSchool\Api\Request;
 use Illuminate\Support\Facades\Facade;
 use GrantHolle\PowerSchool\Api\RequestBuilder;
 
+/**
+ * @method static Request getRequest()
+ * @method static void freshen()
+ * @method static RequestBuilder setTable(string $table)
+ * @method static RequestBuilder table(string $table)
+ * @method static RequestBuilder forTable(string $table)
+ * @method static RequestBuilder againstTable(string $table)
+ * @method static RequestBuilder setId($id)
+ * @method static RequestBuilder id($id)
+ * @method static RequestBuilder forId($id)
+ * @method static mixed resource(string $endpoint, string $method = null, array $data = [])
+ * @method static RequestBuilder excludeProjection()
+ * @method static RequestBuilder setEndpoint(string $endpoint)
+ * @method static RequestBuilder toEndpoint(string $endpoint)
+ * @method static RequestBuilder to(string $endpoint)
+ * @method static RequestBuilder endpoint(string $endpoint)
+ * @method static RequestBuilder setNamedQuery(string $query, array $data = [])
+ * @method static RequestBuilder namedQuery(string $query, array $data = [])
+ * @method static RequestBuilder powerQuery(string $query, array $data = [])
+ * @method static RequestBuilder pq(string $query, array $data = [])
+ * @method static RequestBuilder setData(array $data)
+ * @method static RequestBuilder withData(array $data)
+ * @method static RequestBuilder with(array $data)
+ * @method static RequestBuilder withQueryString($queryString)
+ * @method static RequestBuilder query($queryString)
+ * @method static RequestBuilder addQueryVar(string $key, $val)
+ * @method static bool hasQueryVar(string $key)
+ * @method static RequestBuilder q(string $query)
+ * @method static RequestBuilder projection($projection)
+ * @method static RequestBuilder pageSize(int $pageSize)
+ * @method static RequestBuilder count()
+ * @method static RequestBuilder raw()
+ * @method static mixed asResponse()
+ * @method static mixed send()
+ * @method static RequestBuilder setMethod(string $method)
+ * @method static RequestBuilder method(string $method)
+ * @method static mixed get()
+ * @method static mixed post()
+ * @method static mixed put()
+ * @method static mixed patch()
+ * @method static mixed delete()
+ * @method static mixed getDataSubscriptionChanges(string $applicationName, int $version)
+ * @method static RequestBuilder dataVersion(int $version, string $applicationName)
+ * @method static RequestBuilder includeCount()
+ * @method static RequestBuilder page(int $page)
+ * @method static RequestBuilder sort($columns, bool $descending = false)
+ * @method static RequestBuilder adHocOrder(string $expression)
+ * @method static RequestBuilder order(string $expression)
+ * @method static RequestBuilder queryExpression(string $expression)
+ * @method static RequestBuilder adHocFilter(string $expression)
+ * @method static RequestBuilder filter(string $expression)
+ * @method static void buildRequestJson()
+ * @method static void buildRequestQuery()
+ */
 class PowerSchool extends Facade
 {
     /**
--- src/Request.php
+++ src/Request.php
@@ -3,6 +3,8 @@
 namespace GrantHolle\PowerSchool\Api;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Facades\Cache;
 use GrantHolle\PowerSchool\Api\Exception\MissingClientCredentialsException;
 use Illuminate\Support\Facades\Response;
@@ -12,23 +14,17 @@ class Request
     /* @var string */
     public const AUTH_TOKEN = 'authToken';
 
-    /* @var string */
-    public const CLIENT_ID = 'clientId';
-
-    /* @var string */
-    public const CLIENT_SECRET = 'clientSecret';
-
     /* @var Client */
-    private $client;
+    protected $client;
 
     /* @var string */
-    private $clientId;
+    protected $clientId;
 
     /* @var string */
-    private $clientSecret;
+    protected $clientSecret;
 
     /* @var string */
-    private $authToken;
+    protected $authToken;
 
     /**
      * Creates a new Request object to interact with PS's api
@@ -54,6 +50,7 @@ class Request
      * @param bool $returnResponse Return a response or just decoded
      * @return mixed
      * @throws MissingClientCredentialsException
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function makeRequest(string $method, string $endpoint, array $options, bool $returnResponse = false)
     {
@@ -70,20 +67,28 @@ class Request
         // Add the auth token for the header
         $options['headers']['Authorization'] = 'Bearer ' . $this->authToken;
 
-        // Don't throw exceptions for 4xx and 5xx errors
-        $options['http_errors'] = false;
+        // Throw exceptions for 4xx and 5xx errors
+        $options['http_errors'] = true;
 
-        $response = $this->client->request($method, $endpoint, $options);
+        try {
+            $response = $this->getClient()
+                ->request($method, $endpoint, $options);
+        } catch (ClientException $exception) {
+            $response = $exception->getResponse();
 
-        // If the response is an expired token, reauthenticate and try again
-        if ($response->getStatusCode() === 401) {
-            $wwwHeader = $response->getHeader('WWW-Authenticate');
+            // If the response is an expired token, reauthenticate and try again
+            if ($response->getStatusCode() === 401) {
+                $wwwHeader = $response->getHeader('WWW-Authenticate');
 
-            if (strpos($wwwHeader[0], 'expired') !== false) {
-                return $this->authenticate(true)
-                    ->makeRequest($method, $endpoint, $options);
+                if (strpos($wwwHeader[0], 'expired') !== false) {
+                    return $this->authenticate(true)
+                        ->makeRequest($method, $endpoint, $options);
+                }
             }
+
+            throw $exception;
         }
+
 
         $body = json_decode($response->getBody()->getContents());
 
@@ -122,10 +127,11 @@ class Request
         ];
 
         // Retrieve the access token
-        $response = $this->client->post('/oauth/access_token', [
-            'headers' => $headers,
-            'body' => 'grant_type=client_credentials'
-        ]);
+        $response = $this->getClient()
+            ->post('/oauth/access_token', [
+                'headers' => $headers,
+                'body' => 'grant_type=client_credentials'
+            ]);
 
         $json = json_decode($response->getBody()->getContents());
 
@@ -134,5 +140,13 @@ class Request
         Cache::put(self::AUTH_TOKEN, $this->authToken, now()->addSeconds($json->expires_in));
 
         return $this;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient()
+    {
+        return $this->client;
     }
 }
